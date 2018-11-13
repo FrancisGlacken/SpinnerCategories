80b0faaabb0a5c992a0f797bbe20f7a904195d0e
Francis <francisglacken@gmail.com>
2018-11-13 03:47:08 -0500

    Third iteration, I've added SharedPreference functionality for storing the categories past app termination

--- app/src/main/java/com/deltorostudios/myapplication/MainActivity.java
+++ app/src/main/java/com/deltorostudios/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.deltorostudios.myapplication;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,24 +12,33 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 
 
 /**
  *      This program is a testing ground for a spinner drop down menu
- *  that can be added to or removed by the user.
+ *      that can be added to or removed by the user. The spinner menu
+ *      items will persist past app termination.
  */
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner spinner;
-    private ArrayList<String> list;
-    private int categoryPosition;
+    private ArrayList<String> list = new ArrayList<>();
     private EditText editTextCategory;
-    ArrayAdapter<String> dataAdapter;
+    private ArrayAdapter<String> dataAdapter;
 
 
-
+    /**
+     * onCreate Method
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,17 +48,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner = findViewById(R.id.spinner);
         editTextCategory = findViewById(R.id.editTextCategory);
 
-        // Make a list and add "Exercise" to it
-        list = new ArrayList<String>();
-        list.add("Exercise");
+
+        list = getArrayList("key");
+
+
+
 
         // Make an ArrayAdapter to populate the Spinner
         dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
-    }
 
 
+    } // End onCreate
+
+
+    /**
+     * onDestroy method, where we save the list by calling the saveArrayList method
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        saveArrayList(list, "key");
+    } // End onDestroy
+
+
+    /**
+     * BtnSubmit button, for making a toast containing the name of the current category
+     * @param view
+     */
     // Button that shows a toast to the user showing off the category
     public void BtnSubmit(View view) {
         Toast.makeText(MainActivity.this,
@@ -55,6 +85,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * BtnAddToList button, for adding categories to the list
+     * @param view
+     */
     // Button that add a category from the EditText
     public void BtnAddToList(View view) {
 
@@ -74,6 +108,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         dataAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * BtnRemoveFromList button, for removing categories from the list
+     * @param view
+     */
     // Button that removes the currently selected category
     public void BtnRemoveFromList(View view) {
 
@@ -84,14 +122,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // Make an alert dialog
         AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).create();
         dialog.setTitle("Remove category?");
-        dialog.setMessage("Are you sure you want to remove the " + currentCategory + "category?");
+        dialog.setMessage("Are you sure you want tso remove the " + currentCategory + " category?");
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                // Remove the category that is currently in
-                list.remove(currentCategory);
-                // Notify the adapter that the data set has changed which updates the UI
-                dataAdapter.notifyDataSetChanged();
+
+
+                if (!list.contains(null)) {
+                    // Remove the category that is currently in
+                    list.remove(currentCategory);
+                    // Notify the adapter that the data set has changed which updates the UI
+                    dataAdapter.notifyDataSetChanged();
+                }
             }
         });
         dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
@@ -106,6 +148,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+    /**
+     * saveArrayList method, for converting our ArrayList into a json object for storage in shared preferences
+     * @param list
+     * @param key
+     */
+   public void saveArrayList(ArrayList<String> list, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        SharedPreferences.Editor editor = prefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        editor.putString(key, json);
+        editor.apply();
+    }
+
+    /**
+     * getArrayList method, for getting our json object back from shared preferences and converting it
+     * @param key
+     * @return
+     */
+    public ArrayList<String> getArrayList(String key){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        Gson gson = new Gson();
+        String json = prefs.getString(key, null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        return gson.fromJson(json, type);
+
+    }
+
+
     // Currently unused
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
@@ -119,6 +190,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 }
+
+
 
 
 
